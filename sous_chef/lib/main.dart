5b31f272bd1c3617dacf36ae4a09e07ea396674a
Mitchel Charette <mitchelcharette@gmail.com>
2020-10-27 20:10:37 -0400

    Added a bottom app bar with icon buttons

--- sous_chef/lib/main.dart
+++ sous_chef/lib/main.dart
@@ -10,20 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sous Chef',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Sous Chef'),
     );
   }
 }
@@ -69,51 +60,67 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Transform.rotate(
-          angle: 315 * pi / 180,
-          child: Icon(
-            Icons.restaurant_menu,
+        floatingActionButton: Container(
+          height: 65.0,
+          width: 65.0,
+          child: FittedBox(
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: Transform.rotate(
+                angle: 315 * pi / 180,
+                child: Icon(
+                  Icons.restaurant_menu,
+                ),
+              ),
+              //elevation: 5.0,
+            ),
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+            //elevation: 20.0,
+            shape: CircularNotchedRectangle(),
+            child: Container(
+              height: 75,
+              color: Colors.grey[800],
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    iconSize: 30.0,
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 28.0),
+                    icon: Icon(Icons.list),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    iconSize: 30.0,
+                    color: Colors.white,
+                    padding: EdgeInsets.only(right: 28.0),
+                    icon: Icon(Icons.auto_awesome_motion),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                      iconSize: 30.0,
+                      color: Colors.white,
+                      padding: EdgeInsets.only(left: 28.0),
+                      icon: Icon(Icons.search),
+                      onPressed: () {}),
+                  IconButton(
+                    iconSize: 30.0,
+                    color: Colors.white,
+                    padding: EdgeInsets.only(right: 28.0),
+                    icon: Icon(Icons.settings),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )));
   }
 }
