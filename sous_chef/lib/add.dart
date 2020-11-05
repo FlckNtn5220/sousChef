ae3ae7f5a0c6368d815188052be3f8dd0f040b17
Mitchel Charette <mitchelcharette@gmail.com>
2020-11-05 10:40:33 -0500

    Made a dynamic list that also has a name and description

--- sous_chef/lib/add.dart
+++ sous_chef/lib/add.dart
@@ -10,43 +10,54 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
+  //String list
+  List<String> _todoItems = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Sous Chef')),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+              child: Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    enabledBorder: OutlineInputBorder(),
+                  ),
+                  maxLength: 30,
+                ),
+              ),
+            ),
             Container(
               child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 30,
+                minLines: 1,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.transparent),
                     borderRadius: BorderRadius.all(Radius.circular(30.0)),
                   ),
-                  labelText: 'Name',
+                  labelText: 'Description',
                   floatingLabelBehavior: FloatingLabelBehavior.auto,
                   filled: true,
                   fillColor: Colors.grey[200],
                 ),
-                maxLength: 30,
               ),
               padding: EdgeInsets.all(32.0),
             ),
-            Container(
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: 20,
-                minLines: 1,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                  floatingLabelBehavior: FloatingLabelBehavior.auto,
-                  enabledBorder: OutlineInputBorder(),
-                ),
-              ),
-            ),
+            _buildTodoList()
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+            onPressed:
+                _pushAddTodoScreen, // pressing this button now opens the new screen
+            tooltip: 'Add task',
+            child: Icon(Icons.add)),
         bottomNavigationBar: BottomAppBar(
             //elevation: 20.0,
             shape: CircularNotchedRectangle(),
@@ -105,5 +116,75 @@ class _AddState extends State<Add> {
                 ],
               ),
             )));
+  }
+
+  // This will be called each time the + button is pressed
+  void _addTodoItem(String task) {
+    // Only add the task if the user actually entered something
+    if (task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+// Show an alert dialog asking the user to confirm that the task is done
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text('Remove "${_todoItems[index]}"?'),
+              actions: <Widget>[
+                FlatButton(
+                    child: Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()),
+                FlatButton(
+                    child: Text('REMOVE'),
+                    onPressed: () {
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    })
+              ]);
+        });
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText, int index) {
+    return ListTile(
+        title: Text(todoText), onTap: () => _promptRemoveTodoItem(index));
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        MaterialPageRoute(builder: (context) {
+      return Scaffold(
+          appBar: AppBar(title: Text('Add a new task')),
+          body: TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addTodoItem(val);
+              Navigator.pop(context); // Close the add todo screen
+            },
+            decoration: InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
   }
 }
